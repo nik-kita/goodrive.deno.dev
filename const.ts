9071c70c108e6da12e5f4ede1a6a5572abead4a4
nik-kita <nik.deno.dev@gmail.com>
2024-11-03 22:18:04 +0100

    update google drive scopes to less permissions to avoid verification requirements from google

--- const.ts
+++ const.ts
@@ -1,5 +1,7 @@
 export const GOOGLE_OPEN_ID_SCOPE = "openid";
-export const GOOGLE_GDRIVE_SCOPES = ["https://www.googleapis.com/auth/drive"];
+export const GOOGLE_GDRIVE_SCOPES = [
+  "https://www.googleapis.com/auth/drive.file",
+];
 export const GOOGLE_EMAIL_SCOPE = "email";
 export const GOOGLE_OFFLINE_CONSENT_PARAMS = {
   access_type: "offline",
