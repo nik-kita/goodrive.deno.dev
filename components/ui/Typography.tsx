12b9e1743dd853f9ec2ad76a8e23b542b86df7e0
Nik Kita <nik-kita@on.github>
2024-11-22 20:39:56 +0100

    Typography {...props}

--- components/ui/Typography.tsx
+++ components/ui/Typography.tsx
@@ -5,7 +5,7 @@ type Tag = FunctionComponent<any>; // TODO: type this properly
 
 export const H1: Tag = (props) => {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" {...props}>
       {props.children}
     </h1>
   );
@@ -13,7 +13,7 @@ export const H1: Tag = (props) => {
 
 export const H2: Tag = (props) => {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0" {...props}>
       {props.children}
     </h2>
   );
@@ -21,7 +21,7 @@ export const H2: Tag = (props) => {
 
 export const H3: Tag = (props) => {
   return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight" {...props}>
       {props.children}
     </h3>
   );
@@ -29,7 +29,7 @@ export const H3: Tag = (props) => {
 
 export const H4: Tag = (props) => {
   return (
-    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight" {...props}>
       {props.children}
     </h4>
   );
@@ -37,7 +37,7 @@ export const H4: Tag = (props) => {
 
 export const P: Tag = (props) => {
   return (
-    <p className="leading-7 [&:not(:first-child)]:mt-6">
+    <p className="leading-7 [&:not(:first-child)]:mt-6" {...props}>
       {props.children}
     </p>
   );
@@ -45,7 +45,7 @@ export const P: Tag = (props) => {
 
 export const Blockquote: Tag = (props) => {
   return (
-    <blockquote className="mt-6 border-l-2 pl-6 italic">
+    <blockquote className="mt-6 border-l-2 pl-6 italic" {...props}>
       {props.children}
     </blockquote>
   );
@@ -53,7 +53,7 @@ export const Blockquote: Tag = (props) => {
 
 export const Ul: Tag = (props) => {
   return (
-    <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+    <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props}>
       {props.children}
     </ul>
   );
