a3ff53aebbc92d894641829bfc72fd8e713c305e
nik-kita <nik@kita.github>
2025-03-04 02:03:08 +0100

    allow cors

--- mdw.ts
+++ mdw.ts
@@ -1,13 +1,8 @@
 import { cors } from "hono/cors";
 import { createMiddleware } from "hono/factory";
 import { AppCtx } from "./const.ts";
-import { Env } from "./env.ts";
 
 export const mdw_cors = () => {
-    if (Env.RUNTIME_ENV === "prod" || Env.RUNTIME_ENV === "stage") {
-        return cors({ origin: Env.UI_URL! });
-    }
-
     return cors();
 };
 
